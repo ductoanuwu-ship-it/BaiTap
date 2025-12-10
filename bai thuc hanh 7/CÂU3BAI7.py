68a856c2cc6f138ba3feea534d557b4e542795af
ductoanuwu-ship-it <ductoanuwu@gmail.com>
2025-12-10 07:35:07 +0700

    Delete bai thuc hanh 7 directory

--- bai thuc hanh 7/CÂU3BAI7.py
+++
@@ -1,7 +0,0 @@
-
-file = open('a.txt')
-
-content = file.read()
-print(content)
-
-file.close()
--- bai thuc hanh 7/CÂU6BAI7.py
+++
@@ -1,7 +0,0 @@
-def file_read_from_tail(fname, nlines):
-    with open(fname, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        for line in lines[-nlines:]:
-            print(line.rstrip())
-
-file_read_from_tail('a.txt', 2)
